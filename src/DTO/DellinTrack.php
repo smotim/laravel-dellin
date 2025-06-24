8c70408c6cfe5d2230161b76498fea6bf3d54846
Timofey <timofeykasmolin@gmail.com>
2025-06-24 16:54:49 +0500

    feat: isTerminal in status request

--- src/DTO/DellinTrack.php
+++ src/DTO/DellinTrack.php
@@ -78,8 +78,8 @@ class DellinTrack extends DataTransferObject
         $derivalTerminalId = $data['derival']['terminalId'] ?? null;
         $arrivalTerminalId = $data['arrival']['terminalId'] ?? null;
 
-        $derivalIsTerminal = (bool)$data['orderedDeliveryFromAddress'];
-        $arrivalIsTerminal = (bool)$data['orderedDeliveryToAddress'];
+        $derivalIsTerminal = $data['orderedDeliveryFromAddress'];
+        $arrivalIsTerminal = $data['orderedDeliveryToAddress'];
 
         $link = $orderId ? 'https://www.dellin.ru/tracker/orders/' . $orderId . '/' : '';
 
